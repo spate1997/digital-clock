d95a085e72009f9537709da914c6e82e5090af63
Sheel <spate022@odu.edu>
2024-03-28 19:39:04 -0400

    features

---
+++ src/Clock.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+  state = {
+    time: new Date()
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        {this.state.time.toLocaleTimeString()}
+      </div>
+    );
+  }
+}
+
+export default Clock;
